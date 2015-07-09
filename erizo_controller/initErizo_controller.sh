f075807a1f3e6b7ab4913654fecf3ef77a0e9063
Jay Prall <jay.prall@affectiva.com>
2015-07-09 16:20:25 -0400

    remove AWS environment variables from Erizo_controller init script because they are not set globally

--- erizo_controller/initErizo_controller.sh
+++ erizo_controller/initErizo_controller.sh
@@ -5,12 +5,6 @@ FILENAME=`basename $SCRIPT`
 ROOT=`dirname $SCRIPT`
 CURRENT_DIR=`pwd`
 
-# These all need to be set
-export AWS_REGION='us-east-1'
-export AWS_BUCKET_NAME='aws-bucket-name'
-export AWS_ACCESS_KEY_ID='aws-access-key'
-export AWS_SECRET_ACCESS_KEY='aws-secret-key'
-
 cd $ROOT/erizoController
 node erizoController.js &
 
