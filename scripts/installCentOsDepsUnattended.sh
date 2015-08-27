0444c6b1bd46f420554ee7924c56b95bc7359c7f
toby cabot <toby.cabot@affectiva.com>
2015-08-27 18:01:04 -0400

    Install libsrtp, libnice, and libav from rpm, not source
    libnice for centos is 0.1.3 and licode needs 0.1.4 with a patch, so
    we're building our own rpm and serving it from our internal yum server.
    
    We build an rpm of libsrtp with the diffs between the licode version and
    the upstream version so we no longer need to import the libsrtp source
    from the libsrtp CVS repo.  Checking a cvs working copy into git is
    pretty tacky so it's good that we don't need that anymore.
    
    Both libnice and libsrtp had spec files in Centos, but libav didn't
    since Red Hat distros use ffmpeg.  I found a spec file on github and
    modified it to work with Centos.
    
    All three spec file projects are in our github account: rpms-lib*.

--- scripts/installCentOsDepsUnattended.sh
+++ scripts/installCentOsDepsUnattended.sh
@@ -49,68 +49,7 @@ install_apt_deps(){
   # the packages that we need
   sudo yum -y install epel-release
   # now that yum knows about epel it can install the rest of the packages
-  sudo yum -y install patch git make gcc libvpx-devel opus-devel openssl-devel cmake pkgconfig nodejs glib2-devel boost-devel boost-regex boost-thread boost-system log4cxx-devel rabbitmq-server curl boost-test tar xz libffi-devel npm yasm java-1.7.0-openjdk
-}
-
-install_libnice(){
-  if [ -d $LIB_DIR ]; then
-    cd $LIB_DIR
-    curl -O http://nice.freedesktop.org/releases/libnice-0.1.4.tar.gz
-    tar -xf libnice-0.1.4.tar.gz
-    cd libnice-0.1.4
-    patch -R ./agent/conncheck.c < $PATHNAME/libnice-014.patch0
-    PKG_CONFIG_PATH=${PREFIX_DIR}/lib/pkgconfig ./configure --prefix=$PREFIX_DIR 
-    make -s V=0
-    make install
-    cd $CURRENT_DIR
-  else
-    mkdir -p $LIB_DIR
-    install_libnice
-  fi
-}
-
-install_mediadeps(){
-  cd $LIB_DIR
-  curl -O https://www.libav.org/releases/libav-11.1.tar.gz
-  tar -xf libav-11.1.tar.gz
-  cd libav-11.1
-  PKG_CONFIG_PATH=${PREFIX_DIR}/lib/pkgconfig CPATH=${PREFIX_DIR}/include ./configure --prefix=$PREFIX_DIR --enable-shared --enable-gpl --enable-libvpx --enable-libx264 --enable-libopus
-  make -s V=0
-  make install
-}
-
-install_mediadeps_nogpl(){
-  if [ -d $LIB_DIR ]; then
-    cd $LIB_DIR
-    curl -O https://www.libav.org/releases/libav-11.1.tar.gz
-    tar -xf libav-11.1.tar.gz
-    cd libav-11.1
-    PKG_CONFIG_PATH=${PREFIX_DIR}/lib/pkgconfig CPATH=${PREFIX_DIR}/include ./configure --prefix=$PREFIX_DIR --enable-shared --enable-libopus --enable-libvpx
-    CPATH=${PREFIX_DIR}/include make -s V=0
-    make install
-    cd $CURRENT_DIR
-  else
-    mkdir -p $LIB_DIR
-    install_mediadeps_nogpl
-  fi
-}
-
-install_libsrtp(){
-  cd $ROOT/third_party/srtp
-  CFLAGS="-fPIC" ./configure --prefix=$PREFIX_DIR
-  make -s V=0
-  make uninstall
-  make install
-  cd $CURRENT_DIR
-}
-
-cleanup(){  
-  if [ -d $LIB_DIR ]; then
-    cd $LIB_DIR
-    rm -r libnice*
-    rm -r libav*
-    cd $CURRENT_DIR
-  fi
+  sudo yum -y install patch git make gcc bzip2-devel libav-devel libnice-devel libsrtp-devel libvpx-devel opus-devel openssl-devel cmake pkgconfig nodejs glib2-devel boost-devel boost-regex boost-thread boost-system log4cxx-devel rabbitmq-server curl boost-test tar xz libffi-devel npm yasm java-1.7.0-openjdk
 }
 
 parse_arguments $*
@@ -120,18 +59,3 @@ mkdir -p $PREFIX_DIR
 install_apt_deps
 
 check_proxy
-
-install_libnice
-
-install_libsrtp
-
-if [ "$ENABLE_GPL" = "true" ]; then
-  install_mediadeps
-else
-  install_mediadeps_nogpl
-fi
-
-if [ "$CLEANUP" = "true" ]; then
-  echo "Cleaning up..."
-  cleanup
-fi
